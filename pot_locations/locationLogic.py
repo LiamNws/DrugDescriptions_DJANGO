70eea8249238225eb01492a1d100c3891e9f52fc
Liam Newson <liamnewson83@gmail.com>
2023-10-29 18:25:02 +0000

    working functionality - compares search input against excel file and returns matches

---
+++ pot_locations/locationLogic.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+
+def process_location(search_term):
+    # excel = os.path.join("source", "DrugLocations.xlsx")
+    excel = r"C:/Users/liamn/IdeaProjects/TestSite/PT_portfolio/PT_portfolio\source/DrugLocations.xlsx"
+    try:
+        df = pd.read_excel(excel)
+    except FileNotFoundError:
+        print("File not found.")
+        exit(1)
+    except Exception as e:
+        print("An error has occurred while reading the file")
+        exit(1)
+
+    matching = df[(df["Medication"].str.contains(search_term, case=False, na=False))]
+
+    result = []
+    if not matching.empty:
+        for i, row in matching.iterrows():
+            
+            name = row["Medication"]
+            location = row["Location"]
+            result.append({"name": name, "location": location})
+
+            # result.append(f"{name}: {location}")
+
+    else:
+        result.append("No matches found")
+
+    return result
